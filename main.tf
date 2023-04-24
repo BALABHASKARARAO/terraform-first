2b6f4d78f1dced02b3ad681a4ecdeb82b6818153
BALABHASKARARAO <balabhaskararaoguntupalli@gmail.com>
2023-04-24 13:16:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/24"
-  name = "bala"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
 }
